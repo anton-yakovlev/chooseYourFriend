ea785cbc3b0de4bc81bea30b532d7cd2755b9ddf
Anton Yakovlev <yakovlev.business@gmail.com>
2017-05-23 09:25:22 +0300

    home task #5 karma conf up

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
             fixWebpackSourcePaths: true
         },
         port: 9876,
-        browsers: ['Chrome'], // или Chrome
+        browsers: ['Firefox'], // или Chrome
         captureTimeout: 20000,
         singleRun: true,
         plugins: [
